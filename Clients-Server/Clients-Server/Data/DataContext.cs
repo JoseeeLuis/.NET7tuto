ab4754798081007b1b7774fb44fb5adb13aeca3c
JoseArandaa <jose.aranda.et28@gmail.com>
2024-03-18 12:37:19 -0300

    First Commit

---
+++ Clients-Server/Clients-Server/Data/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Clients_Server.Data
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options) { 
+            
+        }
+    }
+}
---
+++ Clients-Server/Clients-Server/Models/Client.cs
@@ -0,0 +1,11 @@
+﻿namespace Clients_Server.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+           
+    }
+}
