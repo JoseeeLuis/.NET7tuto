78b96134b153a579c155d4814ab00668556dec2c
JoseArandaa <jose.aranda@gm2dev.com>
2024-03-21 17:22:38 -0300

    feat(Models): create Basic WorkerModel

---
+++ Clients-Server/Clients-Server/Models/Worker.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Clients_Server.Models
+{
+    public class Worker
+    {
+        [Key]
+        public int WorkerId { get; set; }
+        public string WorkerName { get; set; } = string.Empty;
+
+        public string SeniorityTypeCode { get; set; } = string.Empty; 
+
+        public DateTime JoiningDate { get; set; } = DateTime.Now;
+           
+    }
+}
