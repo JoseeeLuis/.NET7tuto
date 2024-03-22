8799e16163b2ba129f1b85780384dc213ec3d486
JoseArandaa <jose.aranda@gm2dev.com>
2024-03-22 13:42:15 -0300

    feat(Models):create Address model

---
+++ Clients-Server/Clients-Server/Models/Address.cs
@@ -0,0 +1,16 @@
+﻿namespace Clients_Server.Models
+{
+    public class Address
+    {
+        public Guid AddressId { get; set; }
+        public int WorkerId { get; set; }
+        public Worker Worker { get; set; }
+        public string Street { get; set; } = string.Empty;  
+
+        public int StreetNumber { get; set; }
+
+        public int PostalCode { get; set; }
+
+        public string State { get; set; } = string.Empty;
+    }
+}
