db77d78abed3bec2351f9424d45294b0b1b78405
JoseArandaa <jose.aranda@gm2dev.com>
2024-03-22 09:25:35 -0300

    feat(Models): create Project model

---
+++ Clients-Server/Clients-Server/Models/Project.cs
@@ -0,0 +1,10 @@
+﻿namespace Clients_Server.Models
+{
+    public class Project
+    {
+        public int ProjectId { get; set; }
+        public string ProjectName { get; set; }
+
+        public List<Worker> Workers { get; set; }
+    }
+}
