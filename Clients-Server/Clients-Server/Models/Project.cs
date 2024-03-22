9d0a1d33bc18b7224ed4ba8adfd1f4b167edaeef
JoseArandaa <jose.aranda@gm2dev.com>
2024-03-22 11:54:36 -0300

    Refactor(Project): Change data type of ProjectId from int to Guid

--- Clients-Server/Clients-Server/Models/Project.cs
+++ Clients-Server/Clients-Server/Models/Project.cs
@@ -2,7 +2,7 @@
 {
     public class Project
     {
-        public int ProjectId { get; set; }
+        public Guid ProjectId { get; set; }
         public string ProjectName { get; set; }
 
         public List<Worker> Workers { get; set; }
