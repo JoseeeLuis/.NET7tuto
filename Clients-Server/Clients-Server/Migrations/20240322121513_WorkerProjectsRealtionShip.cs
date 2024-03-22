ab19a8765fccd057e75d8f806bc1e13af9003776
JoseArandaa <jose.aranda@gm2dev.com>
2024-03-22 09:36:00 -0300

    feat(Migrations): Create WorkerProjectRelationship

---
+++ Clients-Server/Clients-Server/Migrations/20240322121513_WorkerProjectsRealtionShip.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Clients_Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class WorkerProjectsRealtionShip : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Projects",
+                columns: table => new
+                {
+                    ProjectId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProjectName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Projects", x => x.ProjectId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProjectWorker",
+                columns: table => new
+                {
+                    ProjectsProjectId = table.Column<int>(type: "int", nullable: false),
+                    WorkersWorkerId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProjectWorker", x => new { x.ProjectsProjectId, x.WorkersWorkerId });
+                    table.ForeignKey(
+                        name: "FK_ProjectWorker_Projects_ProjectsProjectId",
+                        column: x => x.ProjectsProjectId,
+                        principalTable: "Projects",
+                        principalColumn: "ProjectId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_ProjectWorker_Workers_WorkersWorkerId",
+                        column: x => x.WorkersWorkerId,
+                        principalTable: "Workers",
+                        principalColumn: "WorkerId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProjectWorker_WorkersWorkerId",
+                table: "ProjectWorker",
+                column: "WorkersWorkerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProjectWorker");
+
+            migrationBuilder.DropTable(
+                name: "Projects");
+        }
+    }
+}
