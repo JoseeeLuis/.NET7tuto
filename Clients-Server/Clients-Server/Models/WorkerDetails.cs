2972b59bbf11afda7f5d1d49d04d7b679fba3157
JoseArandaa <jose.aranda@gm2dev.com>
2024-03-22 13:14:14 -0300

    feat(Models): create WorkerDetails model

---
+++ Clients-Server/Clients-Server/Models/WorkerDetails.cs
@@ -0,0 +1,15 @@
+﻿namespace Clients_Server.Models
+{
+    public class WorkerDetails
+    {
+        public int WorkerDetailsId { get; set; }
+        public int WorkerId { get; set; }
+        public Worker Worker { get; set; }
+        public DateTime JoiningDate { get; set; } = DateTime.Now;
+        public decimal Salary { get; set; }
+        public string DepartamentTypeCode { get; set; }
+        public string SeniorityTypeCode { get; set; } = string.Empty;
+    }
+}
+
+
