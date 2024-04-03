355a82b5fb90e9dbd4f3eaab1f7ae58db11a3b05
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-03 12:04:02 -0300

    feat(models): Create PostWorkerDTO for transferring worker creation data

---
+++ Clients-Server/Clients-Server/Models/PostWorkerDTO.cs
@@ -0,0 +1,16 @@
+﻿namespace Clients_Server.Models
+{
+    public class PostWorkerDTO
+    {
+        //public int WorkerId { get; set; }
+        public string WorkerName { get; set; }
+        //public int WorkerDetailsId { get; set; }
+        public string Street { get; set; } 
+        public int StreetNumber { get; set; }
+        public int PostalCode { get; set; }
+        public string State { get; set; } 
+        public DateTime JoiningDate { get; set; }= DateTime.Now;
+        public string DepartamentTypeCode { get; set; }
+        public string SeniorityTypeCode { get; set; }
+    }
+}
