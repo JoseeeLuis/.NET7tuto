eb47fc0155f4f9046663ba4f73a4f563285952ab
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-03 11:54:25 -0300

    feat(Models): Add DepartamentType model

---
+++ Clients-Server/Clients-Server/Models/SeniorityType.cs
@@ -0,0 +1,8 @@
+﻿namespace Clients_Server.Models
+{
+    public class SeniorityType
+    {
+        public  string SeniorityTypeCode { get; set; }
+        public  string Seniority {  get; set; }
+    }
+}
