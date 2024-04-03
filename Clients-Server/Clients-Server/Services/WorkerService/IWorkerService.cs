ad9b8eba9240029333dd9c2047227b8af49eceef
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-03 12:42:43 -0300

    feat(Services): Define WorkerService interface methods

---
+++ Clients-Server/Clients-Server/Services/WorkerService/IWorkerService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Clients_Server.Services.WorkerService
+{
+    public interface IWorkerService
+    {
+        Task<List<WorkerDTO>> GetAllWorkers();
+        Task<WorkerDTO> GetSingleWorker(int WorkerId);
+        //Task<List<Worker>> DeleteWorker(int WorkerId);
+
+        Task<List<WorkerDTO>> CreateWorker(PostWorkerDTO postWorkerDTO);
+        //Task<List<Worker>> GetByDepartament(string DepartamentTypeCode);
+        //Task<List<Worker>> GetBySeniority(string SeniorityTypeCode);
+        //Task<List<Worker>> GetByProject(int ProjectId);
+    }
+}
