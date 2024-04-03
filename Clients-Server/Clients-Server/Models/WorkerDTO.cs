1476087c157877be3ecbe84009d532dea3575436
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-03 12:14:05 -0300

    feat(Models): create WorkerDTO model

---
+++ Clients-Server/Clients-Server/Models/WorkerDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace Clients_Server.Models
+{
+    public class WorkerDTO
+    {
+        public int WorkerId { get; set; }
+        public string WorkerName { get; set; } 
+        public string FullAddress { get; set; }
+        public DateTime JoiningDate { get; set; }
+        public string Departament { get; set; } 
+        public string Seniority { get; set; }
+        public List<Project> Projects { get; set; }
+    }
+}
