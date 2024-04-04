596a8f54ae72586d5a7e6734ba271d3ef2f5d632
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-04 09:36:18 -0300

    feat(Repositories): Create WorkerRepository and AddWorker function

---
+++ Clients-Server/Clients-Server/Repositories/WorkerRepository.cs
@@ -0,0 +1,22 @@
+﻿using Clients_Server.Data;
+
+namespace Clients_Server.Repositories
+{
+    public class WorkerRepository : IWorkerRepository
+    {
+        private readonly DataContext _context;
+
+        public WorkerRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Worker> AddWorkerAsync(Worker worker)
+        {
+            _context.Workers.Add(worker);
+            await _context.SaveChangesAsync();
+            return worker;
+        }
+    }
+}
+    
