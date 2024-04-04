9e4371b509e8d4e9b7e00794ad370289ae711d3f
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-04 17:47:14 -0300

    feat(WorkerRepository): add logic for get an delete worker

--- Clients-Server/Clients-Server/Repositories/IWorkerRepository.cs
+++ Clients-Server/Clients-Server/Repositories/IWorkerRepository.cs
@@ -1,7 +1,12 @@
-﻿namespace Clients_Server.Repositories
+﻿using Clients_Server.Models;
+
+namespace Clients_Server.Repositories
 {
     public interface IWorkerRepository
     {
         Task<Worker> AddWorkerAsync(Worker worker);
+        Task<List<Worker>> GetAllWorkersAsync();
+        Task<int> DeleteWorkerAsync(Worker worker);
+        Task<Worker> GetSingleWorkerAsync(int WorkerId);
     }
 }
--- Clients-Server/Clients-Server/Repositories/WorkerRepository.cs
+++ Clients-Server/Clients-Server/Repositories/WorkerRepository.cs
@@ -1,4 +1,6 @@
 ﻿using Clients_Server.Data;
+using Clients_Server.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace Clients_Server.Repositories
 {
@@ -11,12 +13,43 @@ namespace Clients_Server.Repositories
             _context = context;
         }
 
+        public async Task<List<Worker>> GetAllWorkersAsync()
+        {
+            var workers = await _context.Workers
+                .Include(w => w.Address)
+                .Include(w => w.Projects)
+                .Include(w => w.WorkerDetails)
+                .Include(w => w.WorkerDetails.DepartamentType)
+                .Include(w => w.WorkerDetails.SeniorityType)
+                .Where(w => !w.IsDelete)
+                .ToListAsync();
+            return workers;
+        }
         public async Task<Worker> AddWorkerAsync(Worker worker)
         {
             _context.Workers.Add(worker);
             await _context.SaveChangesAsync();
             return worker;
         }
+        public async Task<Worker> GetSingleWorkerAsync(int WorkerId)
+        {
+            var worker = await _context.Workers
+                .Include(w => w.WorkerDetails)
+                .Include(w => w.Address)
+                .Include(w => w.Projects)
+                .Include(w => w.WorkerDetails)
+                .Include(w => w.WorkerDetails.DepartamentType)
+                .Include(w => w.WorkerDetails.SeniorityType)
+                .FirstOrDefaultAsync(w => w.WorkerId == WorkerId);
+
+            return worker;
+        }
+        public async Task<int> DeleteWorkerAsync(Worker worker){
+            worker.IsDelete = true;
+            await _context.SaveChangesAsync();
+            return worker.WorkerId;
+        }
+
     }
 }
     
