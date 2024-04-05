36752c0a1ca524d024ea6b749212643a27994b8e
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-05 13:10:55 -0300

    refactor(IWorkerRepository): Change the return type of DeleteWorkerAsync method from Int to Boolean and change the parameter of the function to int workerId

--- Clients-Server/Clients-Server/Repositories/IWorkerRepository.cs
+++ Clients-Server/Clients-Server/Repositories/IWorkerRepository.cs
@@ -1,12 +1,10 @@
-﻿using Clients_Server.Models;
-
-namespace Clients_Server.Repositories
+﻿namespace Clients_Server.Repositories
 {
     public interface IWorkerRepository
     {
         Task<Worker> AddWorkerAsync(Worker worker);
         Task<List<Worker>> GetAllWorkersAsync();
-        Task<int> DeleteWorkerAsync(Worker worker);
+        Task<Boolean> DeleteWorkerAsync(int workerId);
         Task<Worker> GetSingleWorkerAsync(int WorkerId);
     }
 }
