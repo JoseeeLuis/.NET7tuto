a4b280eb08bf76b4e0f5f0b7b528d702b2791517
JoseArandaa <jose.aranda@gm2dev.com>
2024-04-05 13:38:06 -0300

    feat(Worker): move ToDTO function to WorkerDTO

--- Clients-Server/Clients-Server/Models/Worker.cs
+++ Clients-Server/Clients-Server/Models/Worker.cs
@@ -1,7 +1,4 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-
-namespace Clients_Server.Models
+﻿namespace Clients_Server.Models
 {
     public class Worker
     {
@@ -16,20 +13,6 @@ namespace Clients_Server.Models
         public List<Project> Projects { get; set; }
 
         public Boolean IsDelete { get; set; } = false;
-
-        public WorkerDTO ToDto()
-        {
-            return new()
-            {
-                WorkerId = WorkerId,
-                WorkerName = WorkerName,
-                FullAddress =  $"{Address.Street} {Address.StreetNumber},C.P: {Address.PostalCode}, {Address.State}" ,
-                JoiningDate =  WorkerDetails.JoiningDate,
-                Departament = WorkerDetails.DepartamentType.Departament ,
-                Seniority =  WorkerDetails.SeniorityTypeCode,
-                Projects = Projects
-            };
-        }
 
     }
 }
